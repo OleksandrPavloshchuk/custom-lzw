f26e25bec19f8fac8e83d7d1a2c0d983f54610f4
Oleksandr Pavloshchuk <ap730518@gmail.com>
2018-11-20 12:55:55 +0200

    refactoring

--- main.go
+++ main.go
@@ -11,25 +11,19 @@ import (
 func call(transform func([]byte) ([]byte, error)) {
 	read := config.GetReader()
 	write := config.GetWriter()
-
-	if src, err := read(); err == nil {
-		if res, err := transform(src); err == nil {
-			if err := write(res); err == nil {
-				os.Exit(0)
-			} else {
-				printErrorAndExit(err)
-			}
-		} else {
-			printErrorAndExit(err)
-		}
-	} else {
-		printErrorAndExit(err)
+    src, err := read()
+	if err == nil {
+	    var res []byte
+	    res, err = transform(src)
+	    if err == nil {
+	        err = write(res)
+	        if err == nil {
+				os.Exit(0)	            
+	        }
+	    }
 	}
-}
-
-func printErrorAndExit(err error) {
-	fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
-	os.Exit(2)
+    fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+	os.Exit(2)	
 }
 
 func main() {
